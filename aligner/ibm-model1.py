e96a9a8fc0e3b825a1f7ee67938183a9446f0ffa
motian12ps <chentianyi@ChenTianyis-MacBook-Pro-2.local>
2016-02-22 20:50:29 -0500

    ibm-model1.py

--- aligner/ibm-model1.py
+++ aligner/ibm-model1.py
@@ -35,110 +35,113 @@ e_data = "%s.%s" % (opts.train, opts.english)
 sys.stderr.write("Start Training...\n")
 bitext = [[sentence.strip().split() for sentence in pair] for pair in zip(open(f_data), open(e_data))[:opts.num_sents]]
 
-sys.stderr.write("Get Word Counting...\n")
-f_count  = defaultdict(int) #define a dict
-e_count  = defaultdict(int) #define a dict
-fe_count = defaultdict(int)
+#sys.stderr.write("Get Word Counting...\n")
 
-for (n, (f, e)) in enumerate(bitext):
-  for f_i in set(f):
-    f_count[f_i] += 1
-    for e_j in set(e):
-      fe_count[(f_i,e_j)] += 1
-  for e_j in set(e):
-    e_count[e_j] += 1
-  if n % 500 == 0:
-    sys.stderr.write(".")
+f_list   = []
+e_list   = []
+
+for ( f, e ) in bitext:
+	for f_i in set(f):
+		if f_i not in f_list:
+			f_list.append(f_i)
+	for e_j in set(e):
+		if e_j not in e_list:
+			e_list.append(e_j)
 
 # Map index 
 e_idx    = {}
 f_idx    = {}
 
-for (n, key) in enumerate(f_count):
-	f_idx[key] = n
+for (n, f_i) in enumerate(f_list):
+	f_idx[ f_i ] = n
 
-for (n, key) in enumerate(e_count):
-	e_idx[key] = n
+for (n, e_j) in enumerate(e_list):
+	e_idx[ e_j ] = n
 
 #print f_idx
 #print e_idx
 
 # Get number of distinct f, number of distinct e
-num_f    = len(f_count)
-num_e    = len(e_count)
+num_f    = len(f_list)
+num_e    = len(e_list)
 
 # Define probability t(e|f)
 tef      = np.zeros( ( num_e, num_f ) )
-#tef.shape = (22231, 28686)
+#print num_f, num_e
 
 # Define new probability in E-step
 tnef     = np.zeros( ( num_e, num_f ) )
 # Initialize probabilities uniformly
 tnef.fill( float(1) / float(num_f) )
+#print tnef
 
 # tolerance for convergence
 tol      = 1e-03
 # iteration, and maximum iteration for training
-max_iter = 1
+max_iter = 10
 it       = 0 
 
-print "\n"
+#print "\n"
 
 while True :
 
-	print "Training epoch: "+ str(it)
+	#print "Training epoch: "+ str(it)
 	# Consider termination:
 	if it >= max_iter:
-		sys.stderr.write("Maximum training iteration has been reached.\n")
+		#sys.stderr.write("Maximum training iteration has been reached.\n")
 		break
 
 	'''Check probabilities convergence is too time-consuming. So skip it'''
-	# if LA.norm( tnef - tef ) < tol:
-	# 	sys.stderr.write("Probabilities have converged.\n")
-	# 	break
+	if LA.norm( tnef - tef ) < tol:
+		#sys.stderr.write("Probabilities have converged.\n")
+		break
 
 	tef      = tnef
 
 	'''E-step'''
 	# Initialize Count for C(e|f)
-	print "	E-step:"
-	print "		Initialize Count for C(e|f)"
+	#print "	E-step:"
+	#print "		Initialize Count for C(e|f)"
 	count_ef = np.zeros( ( num_e, num_f ) )
 	total_f  = np.zeros( num_f )
 
-	print "		Compute normalization . Collect counts"
+	#print "		Compute normalization . Collect counts"
 	for (n, (f, e)) in enumerate(bitext):
 		# Compute normalization
-		for e_word in set(e):
-			s_total_e = 0.0
-			for f_word in set(f):
-				s_total_e += tef[ e_idx[e_word] ][ f_idx[f_word] ]
+		s_total_e = {}
+		for e_j in set(e):
+			s_total_e[ e_j ] = 0.0
+			for f_i in set(f):
+				s_total_e[ e_j ] += tef[ e_idx[e_j] ][ f_idx[f_i] ]
+
 		# Collect counts
-		for e_word in set(e):
-			for f_word in set(f):
-				count_ef[ e_idx[e_word] ][ f_idx[f_word] ] += tef[ e_idx[e_word] ][ f_idx[f_word] ] / s_total_e
-				total_f[ f_idx[f_word] ] += tef[ e_idx[e_word] ][ f_idx[f_word] ] / s_total_e
-				
+		for e_j in set(e):
+			for f_i in set(f):
+				count_ef[ e_idx[e_j] ][ f_idx[f_i] ] += tef[ e_idx[e_j] ][ f_idx[f_i] ] / s_total_e[ e_j ]
+				total_f[ f_idx[f_i] ] += tef[ e_idx[e_j] ][ f_idx[f_i] ] / s_total_e[ e_j ]
 
 	'''M-step'''
 	#Estimate probabilities
-	print "	M-step:"
-	print "		Estimate probabilities"
+	#print "	M-step:"
+	#print "		Estimate probabilities"
 	total_f[ total_f == 0 ] = np.inf
 	tnef                    = ( count_ef / total_f )
 
 	it += 1
 
-pef = tnef
+prob_ef = tnef
 
 # Output Word Alignment
 for ( f, e ) in bitext:
 	for ( i, f_i ) in enumerate(f):
-		max_j = np.argmax( pef[ : , f_idx[f_i] ] )
-		sys.stdout.write("%i-%i " % (i,max_j))
-	sys.stdout.write("\n")
-
-	
+		max_idx_j = 0
+		max_prob  = 0.0
+		for ( j, e_j ) in enumerate(e):
+			if prob_ef[ e_idx[e_j] ][ f_idx[f_i] ] > max_prob:
+				max_idx_j = j
+				max_prob  = prob_ef[ e_idx[e_j] ][ f_idx[f_i] ]
+		sys.stdout.write( "%i-%i " % ( i, max_idx_j ) )
+	sys.stdout.write("\n")	
 
 
 
